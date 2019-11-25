859ab360a16870af184108d0d43fd13b4dbb5489
Vadim Kolesov <vvaaddiimm999@gmail.com>
2019-11-25 14:21:19 +0300

    menu fix

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 function burgerClick(){
 	let menu = $('.side-menu')[0];
 	switchClass(menu, 'side-menu-opened');
-	menu.style = `top:${100+$('body')[0].scrollTop}px;`
+	menu.style = `top:${120+$('body')[0].scrollTop}px;`
 	switchClass($('.burger')[0], 'burger-opened');
 }
