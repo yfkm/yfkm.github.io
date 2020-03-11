4ff221db4ee77e9ea548daef3de5ec8458354afc
Vadim Kolesov <vvaaddiimm999@gmail.com>
2020-03-11 11:52:31 +0300

    Updates

---
+++ precache-manifest.e10fcf973073c63c13bb7fff87043ec2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1514a33dbaee507a803f6419edde0e3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f32e3b3190fcaa460bd0",
+    "url": "/static/css/main.7614c839.chunk.css"
+  },
+  {
+    "revision": "8708bc4bc673351335d0",
+    "url": "/static/js/2.bba5cb51.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.bba5cb51.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f32e3b3190fcaa460bd0",
+    "url": "/static/js/main.6b21cf5f.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  }
+]);
